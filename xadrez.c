5c3e2b3b42883eafd410847f44d08cb5aeb56ce0
RichardWSPereira <srichardwilliam@hotmail.com>
2025-05-15 23:52:38 +0000

    Nivel Aventureiro: Movimentação do Cavalo

--- xadrez.c
+++ xadrez.c
@@ -32,23 +32,48 @@ int main() {
         printf("Direita \n");
         torre++;
     }
+    printf("\n");
 
     do {
         printf("Bispo - %dº movimento: ",bispo);
         printf("Cima, Direita \n");
         bispo++;
     } while (bispo<=5);
+    printf("\n");
 
     for (int rainha = 1; rainha <= 8; rainha++) {
         printf("Rainha - %dº movimento: ",rainha);
         printf("Esquerda \n");
     }
+    printf("\n");
 
-    
-
-    // Nível Aventureiro - Movimentação do Cavalo
+     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+
+    /*
+     Com o movimento do cavalo teve que usar loops aninhados, pois o cavalo movimento duas vezes para o mesmo lugar e depois da uma direção diferente, formando um L 
+     Assim, utliza-se o for para destinar o primeiro loop sendo o movimento único fechando o L do movimento
+     Dentro do primeiro loop temos o segundo que descreve os primeiros movimento do cavalo para a mesma direção
+     Nesta estrutura, o primeiro loop teve como condição a variável cavalo ser positiva, sendo que foi declarado inicialmente como 1, tendo decrescido 1 unidade ficou zero, o que configurou apenas um movimento
+     Já o segundo loop foi utilizado o do while, sendo repetido duas vezes antes de ir para o primeiro loop, descrevendo os primeiros movimentos do cavalo
+     Neste segundo loop utilizou a variavel com ganho de uma unidade, sendo declarada como zero e limitada para o loop enquanto seja menor que 2, assim, tivemos dois movimentos seguidos do cavalo para a mesma direção
+     
+    
+    */
+
+    for (int cavalo = 1; cavalo > 0; cavalo--) {
+        int cavalo2 = 0;
+        do {
+            printf("Baixo \n");
+            cavalo2++;
+        } while (cavalo2 < 2);
+        printf("Esquerda\n");
+        printf("\n");
+    } 
+    
+
+   
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
